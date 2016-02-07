3cd8c7b0a9e1ceb2ffc672228a6958ebefd325bd
Daria <testDaria@gmail.com>
2016-02-07 18:13:45 +0200

    низ сайбара

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -9,6 +9,73 @@
     		<a href="" class="b-link -color_red -type_icon -type_favorites">Избранное</a>
     	</div>
     </div>
-    <div class="b-sidebar__block">test</div>
+    <div class="b-sidebar__block">
+    	<ul class="b-sidebarMenu">
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Автомобили</a>
+    			<span class="b-sidebarMenu__count">34456</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Города</a>
+    			<span class="b-sidebarMenu__count">356</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Девушки</a>
+    			<span class="b-sidebarMenu__count">35</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Еда</a>
+    			<span class="b-sidebarMenu__count">345</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Животные</a>
+    			<span class="b-sidebarMenu__count">35</span>
+    		</li>
+    		<li class="b-sidebarMenu__item -state_active">
+    			<a href="" class="b-sidebarMenu__link">Интерьер</a>
+    			<span class="b-sidebarMenu__count">335</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Космос</a>
+    			<span class="b-sidebarMenu__count">315</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Любовь</a>
+    			<span class="b-sidebarMenu__count">5</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Макро</a>
+    			<span class="b-sidebarMenu__count">5</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Мужчины</a>
+    			<span class="b-sidebarMenu__count">358</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Пейзажи</a>
+    			<span class="b-sidebarMenu__count">35</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Спорт</a>
+    			<span class="b-sidebarMenu__count">385</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Фантастика</a>
+    			<span class="b-sidebarMenu__count">35</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Фильмы</a>
+    			<span class="b-sidebarMenu__count">3885</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Hi-Tech</a>
+    			<span class="b-sidebarMenu__count">353</span>
+    		</li>
+    		<li class="b-sidebarMenu__item">
+    			<a href="" class="b-sidebarMenu__link">Цветы</a>
+    			<span class="b-sidebarMenu__count">3875</span>
+    		</li>
+    	</ul>
+    </div>
   </div>
 </div>
