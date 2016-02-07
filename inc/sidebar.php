26de1ec783395aeb056f4870fdc8cd9efcad8f98
Daria <testDaria@gmail.com>
2016-02-07 17:47:48 +0200

    верх сайдбара

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,5 +1,14 @@
 <div class="l-sidebar">
   <div class="b-sidebar">
-    test
+    <div class="b-sidebar__block">
+    	<div class="l-btn">
+    		<a href="" class="b-btn -display_block -type_icon">Регистрация</a>
+    	</div>
+    	<div class="l-links">
+    		<a href="" class="b-link -color_green -type_icon -type_login">Войти</a>
+    		<a href="" class="b-link -color_red -type_icon -type_favorites">Избранное</a>
+    	</div>
+    </div>
+    <div class="b-sidebar__block">test</div>
   </div>
 </div>
