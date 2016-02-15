230279dceaf7e0e8a75fdea2d5da1b6bec1de029
Daria <testDaria@gmail.com>
2016-02-15 18:28:40 +0200

    скачать и очистить

--- favorites.php
+++ favorites.php
@@ -18,7 +18,20 @@
 			<div class="l-pageTitle">
 				<h2 class="b-pageTitle">Последние добавленные картинки</h2>
 			</div>
+			<div class="l-panelDualLinks">
+				<ul class="b-panelDualLinks">
+					<li class="b-panelDualLinks__item -type_download">
+						<a href="" class="b-panelDualLinks__btn"></a>
+						<a href="" class="b-panelDualLinks__link">Скачать все</a>
+					</li>
+					<li class="b-panelDualLinks__item -type_clear">
+						<a href="" class="b-panelDualLinks__btn"></a>
+						<a href="" class="b-panelDualLinks__link">Очистить</a>
+					</li>
+				</ul>
+			</div>
 		</div>
+		
 	</div>
 	<main class="b-content" role="main">
 		<?php require('inc/sidebar.php'); ?>
