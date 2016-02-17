60ec02ca6142d0ba003545e3540691731d70fa7c
Daria <testDaria@gmail.com>
2016-02-17 16:46:29 +0200

    detail

--- inc/detail-content.php
+++ inc/detail-content.php
@@ -5,14 +5,90 @@
     </div>
     <div class="b-detail__block">
       <div class="l-detailList">
-        <ul class="b-detailList">
-          <li class="b-detailList__item">
-            <div class="b-detailList__title">Описание:</div>
-            <div class="b-detailList__val"></div>
+        <ul class="b-detailList -type_column">
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Просмотров:</div>
+              <div class="b-detailList__val">3425</div>
+            </div>
           </li>
-          <li class="b-detailList__item">
-            <div class="b-detailList__title">Тэги:</div>
-            <div class="b-detailList__val"></div>
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Скачиваний:</div>
+              <div class="b-detailList__val">12</div>
+            </div>
+          </li>
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Поделиться:</div>
+              <div class="b-detailList__val"></div>
+            </div>
+          </li>
+        </ul>
+        <ul class="b-detailList -type_column">
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Рейтинг:</div>
+              <div class="b-detailList__val">4.3 (2345)</div>
+            </div>
+          </li>
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Добавлено:</div>
+              <div class="b-detailList__val">24 Сен. 2015</div>
+            </div>
+          </li>
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Ваша оценка:</div>
+              <div class="b-detailList__val">
+                <div class="js-rateStarts b-rateStarts">
+                  <input type=hidden name"rating"/>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="b-detail__block">
+      <div class="l-detailList">
+        <ul class="b-detailList">
+          <li class="b-detailList__item -type_description">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Описание:</div>
+              <div class="b-detailList__val">
+                Обои на рабочий стол в отличном качестве  “Очень красивая картинка с ужасно красивой девушкой” и высоком разрешении 1920x1080, 1680x1050, 1024х768 
+              </div>
+            </div>
+          </li>
+          <li class="b-detailList__item -type_tags">
+            <div class="b-detailList__content">
+              <div class="b-detailList__title">Тэги:</div>
+              <div class="b-detailList__val">
+                <div class="l-tagCloud">
+                  <div class="b-tagCloud">
+                    <a href="" class="b-tagCloud__btn">обои</a>
+                    <a href="" class="b-tagCloud__btn">девушка</a>
+                    <a href="" class="b-tagCloud__btn">hd</a>
+                    <a href="" class="b-tagCloud__btn">makeup</a>
+                    <a href="" class="b-tagCloud__btn">красивая</a>
+                    <a href="" class="b-tagCloud__btn">рыжая</a>
+                    <a href="" class="b-tagCloud__btn">девушка</a>
+                    <a href="" class="b-tagCloud__btn">makeup</a>
+                    <a href="" class="b-tagCloud__btn">супермодель</a>
+                    <a href="" class="b-tagCloud__btn">девушка</a>
+                    <a href="" class="b-tagCloud__btn">hd</a>
+                    <a href="" class="b-tagCloud__btn">makeup</a>
+                    <a href="" class="b-tagCloud__btn">красивая</a>
+                    <a href="" class="b-tagCloud__btn">рыжая</a>
+                    <a href="" class="b-tagCloud__btn">девушка</a>
+                    <a href="" class="b-tagCloud__btn">makeup</a>
+                    <a href="" class="b-tagCloud__btn">супермодель</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </li>
         </ul>
       </div>
