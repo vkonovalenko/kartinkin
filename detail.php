3c38666dc4ef29f5fd11b30e47d2d0792866ced0
Daria <testDaria@gmail.com>
2016-02-17 15:27:47 +0200

    detail

---
+++ detail.php
@@ -0,0 +1,36 @@
+<?php
+  $pageName = '';
+  $siteName = 'Картинки на рабочий стол';
+  $uri = 'index';
+  require('inc/header.php');
+?>
+<div class="l-breadCrumbs">
+	<ul class="b-breadCrumbs">
+		<li class="b-breadCrumbs__item">
+			<a href="" class="b-breadCrumbs__link">Секретный проект номер 1</a>
+		</li>
+		<li class="b-breadCrumbs__item">
+			<a href="" class="b-breadCrumbs__link">Девушки</a>
+		</li>
+		<li class="b-breadCrumbs__item">Очень красивая картинка с ужасно красивой девушкой</li>
+	</ul>
+</div>
+<div class="l-content">
+	<div class="l-panel">
+		<div class="b-panel">
+			<div class="l-pageTitle">
+				<h2 class="b-pageTitle">Очень красивая картинка с ужасно красивой девушкой</h2>
+			</div>
+			<?php require('inc/view.php'); ?>
+		</div>
+		
+	</div>
+	<main class="b-content" role="main">
+		<?php require('inc/sidebar.php'); ?>
+		<div class="b-content__content">
+			<?php require('inc/detail-content.php'); ?>
+		</div>
+	</main>
+
+
+<?php require('inc/footer.php'); ?>
---
+++ inc/detail-content.php
@@ -0,0 +1,21 @@
+<div class="l-detail">
+  <div class="b-detail">
+    <div class="b-detail__imgWrapper">
+      <img src="img/userfiles/detail/detail-1.png" alt="" class="b-detail__img">
+    </div>
+    <div class="b-detail__block">
+      <div class="l-detailList">
+        <ul class="b-detailList">
+          <li class="b-detailList__item">
+            <div class="b-detailList__title">Описание:</div>
+            <div class="b-detailList__val"></div>
+          </li>
+          <li class="b-detailList__item">
+            <div class="b-detailList__title">Тэги:</div>
+            <div class="b-detailList__val"></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
